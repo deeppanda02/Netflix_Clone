92a645f716e8e72d0ae30b6d35906dfed3bdbc4e
deeppanda02 <deeppanda5500@gmail.com>
2024-08-27 20:38:33 +0530

    first commit

---
+++ App.js
@@ -0,0 +1,66 @@
+// QUESTION-1
+const targetDiv = document.getElementById('answer1');
+const btn = document.getElementById('question1');
+btn.onclick = function () {
+    if(targetDiv.style.display !== "none"){
+        targetDiv.style.display = "none";
+    }
+    else{
+        targetDiv.style.display = "block";
+    }
+}
+// QUESTION-2
+const targetDiv2 = document.getElementById('answer2');
+const btn2 = document.getElementById('question2');
+btn2.onclick = function () {
+    if(targetDiv2.style.display !== "none"){
+        targetDiv2.style.display = "none";
+    }
+    else{
+        targetDiv2.style.display = "block";
+    }
+}
+// QUESTION-3
+const targetDiv3 = document.getElementById('answer3');
+const btn3 = document.getElementById('question3');
+btn3.onclick = function () {
+    if(targetDiv3.style.display !== "none"){
+        targetDiv3.style.display = "none";
+    }
+    else{
+        targetDiv3.style.display = "block";
+    }
+}
+// QUESTION-4
+const targetDiv4 = document.getElementById('answer4');
+const btn4 = document.getElementById('question4');
+btn4.onclick = function () {
+    if(targetDiv4.style.display !== "none"){
+        targetDiv4.style.display = "none";
+    }
+    else{
+        targetDiv4.style.display = "block";
+    }
+}
+// QUESTION-5
+const targetDiv5 = document.getElementById('answer5');
+const btn5 = document.getElementById('question5');
+btn5.onclick = function () {
+    if(targetDiv5.style.display !== "none"){
+        targetDiv5.style.display = "none";
+    }
+    else{
+        targetDiv5.style.display = "block";
+    }
+}
+// QUESTION-6
+const targetDiv6 = document.getElementById('answer6');
+const btn6 = document.getElementById('question6');
+btn6.onclick = function () {
+    if(targetDiv6.style.display !== "none"){
+        targetDiv6.style.display = "none";
+    }
+    else{
+        targetDiv6.style.display = "block";
+    }
+}
